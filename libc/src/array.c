d22d0f05a031d2470828dea274decb9f4924a6a8
Ptitmorceaux <106253875+ptitmorceaux@users.noreply.github.com>
2026-01-22 03:03:57 +0100

    add comm: /// ALLOCATION && FREE PTR ///

--- libc/src/array.c
+++ libc/src/array.c
@@ -11,6 +11,9 @@ DLLEXPORT unsigned char sum_float32_array(const float* array, int32_t array_leng
     return RES_EXIT_SUCCESS;
 }
 
+
+/// ALLOCATION && FREE PTR ///
+
 DLLEXPORT unsigned char get_float32_array_of_incrementing_numbers(int32_t num_elements, float** res_array) {
     if (!res_array) return ERR_INVALID_PTR;
     float* array = (float*) malloc(num_elements * sizeof(float));
