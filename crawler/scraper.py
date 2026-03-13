2751ec79b7208905da86dd408b6f3f1234fc583d
LananasBTW <abane.zreik@gmail.com>
2026-03-13 08:41:27 +0100

    feat(scraper): implement image downloading functionality and define movements

---
+++ crawler/scraper.py
@@ -0,0 +1,30 @@
+import os
+import csv
+import time
+import requests
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+MOUVEMENTS = {
+    "suréalisme": "https://www.wikiart.org/fr/paintings-by-style/surrealisme",
+    "cubisme": "https://www.wikiart.org/fr/paintings-by-style/cubisme",
+    "early renaissance": "https://www.wikiart.org/fr/paintings-by-style/early-renaissance"
+}
+
+DATASET_DIR = os.path.join(os.path.dirname(__file__), '..', 'dataset')
+
+def download_image(url, folder_path, filename):
+    try:
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            save_path = os.path.join(folder_path, filename)
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+            return True
+    except Exception as e:
+        print(f"Exception lors du téléchargement de l'image : {e}")
+    return False
