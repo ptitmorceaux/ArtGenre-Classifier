9bbbab5b9f79e2cae6c8aeaabe2bf29d2dd72c25
LananasBTW <abane.zreik@gmail.com>
2026-03-22 18:56:40 +0100

    feat(linear_model): clean up and uncomment structure definition for linear model

--- libc/src/linearModel.c
+++ libc/src/linearModel.c
@@ -1,36 +1,27 @@
 #include "../include/linearModel.h"
 
-/**  Structure for a linear model */
+/**  Structure for a linear model **/
+/*
+    # Mettre en place l'équation `y = W X + b` : 
+        - W : poids du modèle
+        - X : features d'entrée
+        - b : biais du modèle
 
+    # Implémenter dans un tableau les poids et les biais du modèle, 
+        Ainsi que les features d'entrée ou la première case du tableau sera le biais
+        -> (w(0) = b) et les cases suivantes seront les poids (w(i) = W(i-1)).
+*/
 
+ /** Fonction de prédiction **/
+ /*
+    # Implémenter 2 fonction de prédiction :
+        - `predict_classification` : pour les tâches de classification, qui retourne la classe prédite (0 ou 1)
+        - `predict_regression` : pour les tâches de régression, qui retourne la valeur
+ */
 
-
-
-// typedef struct {
-//     float *coef;        // tableau des coefficients
-//     float *feature;     // tableau des features
-//     float *intercept;    // intercept
-// } LinearModel;
-
-// // Linear Model functions
-
-// unsigned char create_linear_model(float a, float b, LinearModel** res_model) {
-//     if (!res_model) return ERR_INVALID_PTR;
-//     LinearModel* model = (LinearModel*) malloc(sizeof(LinearModel));
-//     model->coef = a;
-//     model->feature = b;
-//     *res_model = model;
-//     return RES_EXIT_SUCCESS;
-// }
-
-// unsigned char predict_linear_model(LinearModel* model, float* result) {
-//     if (!model || !result) return ERR_INVALID_PTR;
-//     *result = model->a + model->b;
-//     return RES_EXIT_SUCCESS;
-// }
-
-// unsigned char release_linear_model(LinearModel* model) {
-//     if (!model) return ERR_INVALID_PTR;
-//     free(model);
-//     return RES_EXIT_SUCCESS;
-// }
+ /** Fonction d'entraînement **/
+ /*
+    # Implémenter une fonction d'entraînement :
+        - `train_linear_model` : pour entraîner le modèle sur un ensemble de données
+            (Règle de Rosenblatt pour la classification ou descente de gradient pour la régression)
+ */
