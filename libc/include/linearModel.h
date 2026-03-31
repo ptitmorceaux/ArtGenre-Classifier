b113b3a0786d0c9494d2926516e1d9a081410968
LananasBTW <abane.zreik@gmail.com>
2026-03-31 23:59:00 +0200

    feat(linearModel): Enhance LinearModel structure and update model creation functions

--- libc/include/linearModel.h
+++ libc/include/linearModel.h
@@ -3,17 +3,25 @@
 
 
 #include "global.h"
+#include "matrix.h"
+#include "array.h"
+
+typedef struct {
+    float* weights;
+    uint32_t input_dim;
+} LinearModel;
 
 // Allocation et libération
-EXPORT double* create_linear_model(int input_dim);
-EXPORT void free_linear_model(double* model);
+DLLEXPORT unsigned char create_linear_model(uint32_t input_dim, LinearModel** res_model);
+DLLEXPORT unsigned char free_linear_model(LinearModel** model_ptr);
 
 // Prédiction
-EXPORT double predict_classification(double* model, double* input, int input_dim);
-EXPORT double predict_regression(double* model, double* input, int input_dim);
+DLLEXPORT unsigned char predict_classification(LinearModel* model, double* input, uint32_t input_dim);
+DLLEXPORT unsigned char predict_regression(LinearModel* model, double* input, uint32_t input_dim);
 
 // Entraînements
-EXPORT void train_classification();
-EXPORT void train_regression();
+DLLEXPORT unsigned char train_classification(LinearModel* model, double* dataset_inputs, double* dataset_expected_outputs, uint32_t dataset_size, uint32_t alpha, uint32_t epochs);
+DLLEXPORT unsigned char train_regression(LinearModel* model, double* dataset_inputs, double* dataset_expected_outputs, uint32_t dataset_size, uint32_t alpha, uint32_t epochs);
 
 #endif
+    
--- libc/src/linearModel.c
+++ libc/src/linearModel.c
@@ -12,30 +12,45 @@
         -> (w(0) = b) et les cases suivantes seront les poids (w(i) = W(i-1)).
 */
 
-EXPORT double* create_linear_model(int input_dim) {
+unsigned char create_linear_model(uint32_t input_dim, Matrix** res_model) {
     /*
     Crée un modèle linéaire avec des poids et un biais initialisés aléatoirement.
     */
     // Allouer de la mémoire pour les poids et le biais et ajouter 1 pour le biais
-    double *model = (double*)malloc((input_dim + 1) * sizeof(double));
+    if (!res_model) return ERR_INVALID_PTR;
+    
+    LinearModel* model = (LinearModel*) malloc(sizeof(LinearModel));
+    if (!model) return ERR_ALLOCATION_FAILED;
+    model->weights = NULL;
+    model->input_dim = input_dim;
 
-    // Initialisation de l'aléatoire pour les poids et le biais
-    srand(time(NULL));
-
-
-    // Initialiser les poids et le biais avec des valeurs aléatoires entre -1 et 1
-    for (int i = 0; i < input_dim + 1; i++) {
-        model[i] = ((double)rand() / RAND_MAX) * 2 - 1;
+    model->weights = (float*) malloc((input_dim + 1) * sizeof(float)); // +1 pour le biais
+    if (!model->weights) {
+        free_linear_model(&model);
+        return ERR_ALLOCATION_FAILED;
     }
 
-    return model;
+    // Initialisation pour les poids et le biais avec des valeurs aléatoires entre -1 et 1
+    fill_randomly_float_array(-1.0f, 1.0f, model->weights, input_dim + 1);
+    
+    return RES_EXIT_SUCCESS;
 }
 
-EXPORT void free_linear_model(double* model) {
+unsigned char free_linear_model(LinearModel** model_ptr) {
     /*
     Libère la mémoire allouée pour le modèle linéaire.
     */
+    if (!model_ptr || !(*model_ptr)) return ERR_INVALID_PTR;
+    
+    LinearModel* model = *model_ptr;
+    if (model->weights) {
+        free(model->weights);
+    }
+
     free(model);
+
+    *model_ptr = NULL;
+    return RES_EXIT_SUCCESS;
 }
 
  /** Fonction de prédiction **/
@@ -44,10 +59,9 @@ EXPORT void free_linear_model(double* model) {
         - `predict_classification` : pour les tâches de classification, qui retourne la classe prédite (0 ou 1)
         - `predict_regression` : pour les tâches de régression, qui retourne la valeur
  */
-
-
+ 
 // Fonction de prédiction pour la classification
- EXPORT double predict_classification(double* model, double* input, int input_dim) {
+ unsigned char predict_classification(double* model, double* input, int input_dim) {
     /*
     Prédit la classe pour une entrée donnée en utilisant le modèle linéaire (renvoie 0 ou 1).
     */
@@ -61,7 +75,7 @@ EXPORT void free_linear_model(double* model) {
  }
 
 // Fonction de prédiction pour la regréssion
-EXPORT double predict_regression(double* model, double* input, int input_dim) {
+unsigned char predict_regression(double* model, double* input, int input_dim) {
     /*
     Prédit la classe pour une entrée donnée en utilisant le modèle linéaire (renvoie un double).
     */
@@ -95,7 +109,7 @@ EXPORT double predict_regression(double* model, double* input, int input_dim) {
             - Mise à jour des poids : W_i = W_i + (alpha * Erreur * X_i)
             - Mise à jour du biais  : b = b + (alpha * Erreur)
 */
-EXPORT void train_classification(double* models, double* dataset_inputs, double* dataset_expected_outputs, int input_dim, int dataset_size, int alpha, int epochs) {
+unsigned char void train_classification(double* models, double* dataset_inputs, double* dataset_expected_outputs, int input_dim, int dataset_size, int alpha, int epochs) {
     // On boucle sur le nombre d'époques
     for (int i = 0; i < epochs; i++) {
         // On boucle sur le nombre d'exemple dans le dataset
