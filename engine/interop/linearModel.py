fa02280f51b825f6c2da671bd9678c38d9891d15
Abane ZREIK <abane.zreik@sfr.com>
2026-04-01 15:32:48 +0200

    feat(linearModel): Improve LinearModel class documentation and add input_dim validation

--- engine/interop/linearModel.py
+++ engine/interop/linearModel.py
@@ -1,16 +1,25 @@
 import ctypes
+from typing import Type
 from engine.interop.loader import Loader
+
 
 class LinearModel:
     """
     Wrapper Python pour LinearModel en C.
-    Cette classe perrmet de gérer le pointeur mémoire en C
+    Cette classe permet de gérer le pointeur mémoire côté C.
     """
 
+    @staticmethod
+    def _check_range(value, ctype: Type[ctypes._SimpleCData], prefix_errmsg: str) -> None:
+        Loader.check_primitive_values_range(value, ctype=ctype, prefix_errmsg=prefix_errmsg)  # type: ignore[arg-type]
 
     def __init__(self, input_dim: int):
+        LinearModel._check_range(input_dim, ctypes.c_uint32, "LinearModel.__init__()")
+        if input_dim == 0:
+            raise ValueError("LinearModel.__init__(): `input_dim` must be > 0")
+
         self.input_dim = input_dim
-        # On créer un pointeur vide via (void*) qui resevera l'adresse du modèle
+        # On crée un pointeur vide (void*) qui recevra l'adresse du modèle C.
         self.ptr = ctypes.c_void_p()
 
         Loader.call(
