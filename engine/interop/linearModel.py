bd9dbfa554f16bb90ff24b460eaa9b4e07f3ae7e
LananasBTW <abane.zreik@gmail.com>
2026-04-05 19:50:15 +0200

    feat(linearModel): Remove static method _check_range and directly call Loader.check_primitive_values_range in __init__

--- engine/interop/linearModel.py
+++ engine/interop/linearModel.py
@@ -9,12 +9,8 @@ class LinearModel:
     Cette classe permet de gérer le pointeur mémoire côté C.
     """
 
-    @staticmethod
-    def _check_range(value, ctype: Type[ctypes._SimpleCData], prefix_errmsg: str) -> None:
-        Loader.check_primitive_values_range(value, ctype=ctype, prefix_errmsg=prefix_errmsg)  # type: ignore[arg-type]
-
     def __init__(self, input_dim: int):
-        LinearModel._check_range(input_dim, ctypes.c_uint32, "LinearModel.__init__()")
+        Loader.check_primitive_values_range(input_dim, ctypes.c_uint32, "LinearModel.__init__()")
         if input_dim == 0:
             raise ValueError("LinearModel.__init__(): `input_dim` must be > 0")
 
