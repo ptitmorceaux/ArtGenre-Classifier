dc50fd8edc8d5fe4315bb4a4132256774206157f
LananasBTW <abane.zreik@gmail.com>
2026-04-06 21:15:45 +0200

    refactor(mlp): standardize variable naming for consistency in MLP struct

--- libc/include/mlp.h
+++ libc/include/mlp.h
@@ -6,12 +6,12 @@
 #include "random.h"
  
 typedef struct {
-    uint32_t* d;     // Tableau de dimensions (nombre de neuronnes par couche)
-    uint32_t L;              // Nombre de "sauts" entre les couches (len(dimension) - 1)
+    uint32_t* d;            // Tableau de dimensions (nombre de neuronnes par couche)
+    uint32_t L;             // Nombre de "sauts" entre les couches (len(dimension) - 1)
 
     float** W;              // Poids (W[l] est la matrice de poids vers la couche l+1) --> ne pas oublier le biais 
     float** X;              // Activations (X[l] aura les valeurs des neuronnes de la couche l)
-    float** Deltas;              // Deltas (Deltas[l] aura les erreurs de la couche l)
+    float** deltas;         // Deltas (Deltas[l] aura les erreurs de la couche l)
 } MLP;
 
 
