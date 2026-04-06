bef729b2278b855886503b9bbbb80bbcdf009dbe
LananasBTW <abane.zreik@gmail.com>
2026-04-06 20:09:44 +0200

    refactor(mlp): remove commented-out weight array declaration in MLP struct

--- libc/include/mlp.h
+++ libc/include/mlp.h
@@ -9,7 +9,6 @@ typedef struct {
     uint32_t* dimension;     // Tableau de dimensions (nombre de neuronnes par couche)
     uint32_t L;              // Nombre de "sauts" entre les couches (len(dimension) - 1)
 
-    // Tableaux de poids ...
     float** W;              // Poids (W[l] est la matrice de poids vers la couche l+1) --> ne pas oublier le biais 
     float** X;              // Activations (X[l] aura les valeurs des neuronnes de la couche l)
     float** Deltas;              // Deltas (Deltas[l] aura les erreurs de la couche l)
