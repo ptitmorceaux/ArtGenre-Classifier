c3b77c4afdfba3a043827e8f7f5c2d9711751cc5
Abane Zreik <abane.zreik@gmail.com>
2026-04-07 11:31:03 +0200

    fix(scraper): remove unused ChromeDriverManager import and related service initialization

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -4,11 +4,9 @@ import requests
 import csv
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 
 TARGET_IMAGES = 2500
 
