105d04cae6dc635f3e70b8d3f3f39710bae009e7
LananasBTW <abane.zreik@gmail.com>
2026-06-28 20:26:00 +0200

    feat(rbf): Define RBF structure and implement euclidean_distance function

--- libc/include/rbf.h
+++ libc/include/rbf.h
@@ -5,6 +5,13 @@
 #include "global.h"
 #include "array.h"
 #include "random.h"
+#include "linearModel.h"
 
+typedef struct {
+    uint32_t input_dim; // dimension de chaque vecteur d'entrée
+    uint32_t num_centers; // nombre de centres
+    float gamma; // variance de la gaussienne
+    float* centers; // tableau des centres [num_centers * input_dim]
+} RBF;
 
 #endif 
--- libc/src/rbf.c
+++ libc/src/rbf.c
@@ -1,1 +1,31 @@
 #include "../include/rbf.h"
+
+/** ==========================================
+ * Structure for a Radial Basis Function (RBF) Network
+ * ========================================== **/
+/*
+    # Architecture d'un réseau de neurones à fonctions de base radiale (RBF) : 
+        - input_dim : Dimension de chaque vecteur d'entrée.
+        - num_centers : Nombre de centres.
+        - gamma : Variance de la fonction gaussienne.
+        - centers : Tableau des centres [num_centers * input_dim].
+
+    # Implémentation en C :
+        
+*/
+
+unsigned char euclidean_distance(float* a, float* b, uint32_t dim, float* result) {
+    float sum = 0.0f;
+    for (uint32_t i = 0; i < dim; i++) {
+        float diff = a[i] - b[i];
+        sum += diff * diff;
+    }
+
+    *result = sqrtf(sum);
+    return RES_EXIT_SUCCESS;
+}
+
+// Alogorithme de K-Means
+unsigned char rbf_kmeans() {
+    return RES_EXIT_SUCCESS;
+}
