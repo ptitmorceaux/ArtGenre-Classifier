43acfb7bee2cdcf59efdb5fbec865f1243aea4b1
Ptitmorceaux <106253875+ptitmorceaux@users.noreply.github.com>
2026-07-06 09:39:39 +0200

    feat(normalization): implement creation functions for StandardNormalizationData and StandardPerColumnNormalizationData

--- libc/src/normalization.c
+++ libc/src/normalization.c
@@ -1,5 +1,6 @@
 #include "../include/normalization.h"
 
+
 const char* get_normalization_method_string(NormalizationMethod normalization_method) {
     switch (normalization_method) {
         case STANDARD:              return "STANDARD";
@@ -8,10 +9,43 @@ const char* get_normalization_method_string(NormalizationMethod normalization_me
     }
 }
 
+///////////////////////////////////////////////
+
+unsigned char create_StandardNormalizationData(float mean, float std, StandardNormalizationData** data) {
+    if (data == NULL) return ERR_INVALID_PTR;
+    
+    *data = (StandardNormalizationData*) malloc(sizeof(StandardNormalizationData));
+    if (*data == NULL) return ERR_MEMORY_ALLOCATION;
+    
+    (*data)->method = STANDARD;
+    (*data)->mean = mean;
+    (*data)->std = std;
+    
+    return RES_EXIT_SUCCESS;
+}
+
 unsigned char free_StandardNormalizationData(StandardNormalizationData** data) {
     if (data == NULL || *data == NULL) return RES_EXIT_SUCCESS;
     free(*data);
     *data = NULL;
+    return RES_EXIT_SUCCESS;
+}
+
+///////////////////////////////////////////////
+
+unsigned char create_StandardPerColumnNormalizationData(float* mean, float* std, uint32_t* length, StandardPerColumnNormalizationData** data) {
+    if (data == NULL) return ERR_INVALID_PTR;
+    
+    *data = (StandardPerColumnNormalizationData*) malloc(sizeof(StandardPerColumnNormalizationData));
+    if (*data == NULL) return ERR_MEMORY_ALLOCATION;
+
+    // TODO: demander a claude si mean et std changent quand on change sur python (pcque le tableau est alloue sur python)
+     
+    (*data)->method = STANDARD_PER_COLUMN;
+    (*data)->mean = mean;
+    (*data)->std = std;
+    (*data)->length = length;
+    
     return RES_EXIT_SUCCESS;
 }
 
